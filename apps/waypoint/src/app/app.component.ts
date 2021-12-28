9030b9a6f550d489a3ad1afdaec36d030771005f
Yuri Sim <edaraks@gmail.com>
2021-12-28 08:15:04 -0600

    initial commit

---
+++ apps/waypoint/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@odst/api-interfaces';
+
+@Component({
+  selector: 'odst-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
