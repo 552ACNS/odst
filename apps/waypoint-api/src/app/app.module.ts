1bab16c75933f9505d6adbda2f9db37e02b81c5d
Yuri Sim <edaraks@gmail.com>
2021-12-29 08:53:11 -0600

    angular json changes

--- apps/waypoint-api/src/app/app.module.ts
+++ apps/waypoint-api/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
+import { GraphQLModule, registerEnumType } from '@nestjs/graphql';
 import { PersonModule } from '../person/person.module';
 import { OrgModule } from '../org/org.module';
 import { IncidentModule } from '../incident/incident.module';
+import { HairColor, EyeColor, BirthState, Role, Spec, OrgTier } from '@prisma/client';
+import { ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageDisabled
+} from "apollo-server-core";
+// Register enum types here, if they are used in multiple places, make sure that they are registered
+// only once and that the resource module that is imported first is the one that registers them
+registerEnumType(HairColor, { name: 'HairColor' });
+registerEnumType(EyeColor, { name: 'EyeColor' });
+registerEnumType(BirthState, { name: 'BirthState' });
+registerEnumType(Role, { name: 'Role' });
+registerEnumType(Spec, { name: 'Spec' });
+registerEnumType(OrgTier, { name: 'OrgTier' });
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
-      playground: process.env.NODE_ENV !== 'production',
-      // plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      // Disables playground because the apollo server system is better
+      // playground: process.env.NODE_ENV !== 'production',
+      playground: false,
+      plugins: [
+        // Install a landing page plugin based on NODE_ENV
+        process.env.NODE_ENV === 'production'
+          ? ApolloServerPluginLandingPageDisabled()
+          : ApolloServerPluginLandingPageLocalDefault(),
+      ],
     }),
     PersonModule,
     OrgModule,
--- apps/waypoint/src/app/app.component.ts
+++ apps/waypoint/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@odst/types';
 
 @Component({
   selector: 'odst-root',
@@ -8,6 +7,6 @@ import { Message } from '@odst/types';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
+  // hello$ = this.http.get<Message>('/api/hello');
   constructor(private http: HttpClient) {}
 }
