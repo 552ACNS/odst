655c1d2a2f566ba69637c74328381e75ec1301a5
eric.cater <eric.cater@us.af.mil>
2022-01-12 16:29:32 -0600

    auth service tests

--- apps/waypoint-api/src/auth/auth.service.spec.ts
+++ apps/waypoint-api/src/auth/auth.service.spec.ts
@@ -1,5 +1,12 @@
+import { UnauthorizedException } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import {
+  LoginUserInput,
+  SignupUserInput,
+  UserWhereUniqueInput,
+} from '@odst/types';
+import { User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { prismaMock } from '../prisma/singleton';
 import { UserService } from '../user/user.service';
@@ -28,5 +35,39 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should validate user ', async () => {
+    await service.validateUser('thisismyusername', 'thisismypassword');
+    expect(prismaMock.user.findUnique).toHaveBeenCalled();
+  });
+
+  it('should throw UnauthorizedException due to incorrecct credentials', async () => {
+    const userInput: LoginUserInput = {
+      username: 'idonotexist',
+      password: 'password',
+    };
+    const userWhereUniqueInput: UserWhereUniqueInput = {
+      username: userInput.username,
+    };
+    const resolvedUser: User = userWhereUniqueInput as unknown as User;
+    try {
+      await await service.login(resolvedUser);
+    } catch (error) {
+      expect(error).toBeInstanceOf(UnauthorizedException);
+    }
+    expect(prismaMock.user.findUnique).toHaveBeenCalled();
+  });
+
+  it('should sign up user', async () => {
+    const userInput: SignupUserInput = {
+      username: 'test',
+      password: 'test',
+      person: { connect: { dodId: 123456789 } },
+    };
+
+    await service.signup(userInput);
+
+    expect(prismaMock.user.create).toHaveBeenCalled();
   });
 });
