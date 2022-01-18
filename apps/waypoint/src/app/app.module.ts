18102a81ee09d2a2494df17a3597d79923fc8e41
brenden.mcgrew <brenden.mcgrew@us.af.mil>
2022-01-18 12:06:15 -0600

    Added complonent objects and created a form

--- apps/waypoint/src/app/app.module.ts
+++ apps/waypoint/src/app/app.module.ts
@@ -5,11 +5,39 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Apollo } from 'apollo-angular';
 import { GQLModule } from '@odst/gql';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreatePersonComponent } from './create-person/create-person.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, GQLModule],
+  declarations: [AppComponent, CreatePersonComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    GQLModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [Apollo],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
--- apps/waypoint/src/app/create-person/create-person.component.ts
+++ apps/waypoint/src/app/create-person/create-person.component.ts
@@ -1,18 +1,127 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Spec } from '@prisma/client';
+import { HairColor } from '@prisma/client';
+import { EyeColor } from '@prisma/client';
+import { BirthState } from '@prisma/client';
+import { Apollo, gql, Mutation } from 'apollo-angular';
+import { useMutation } from '@apollo/client';
 import { PersonGQL } from '@odst/types';
+import { PersonCreateInput } from '@odst/types';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'odst-create-person',
   templateUrl: './create-person.component.html',
-  styleUrls: ['./create-person.component.scss']
+  styleUrls: ['./create-person.component.scss'],
 })
 export class CreatePersonComponent implements OnInit {
+  specs: string[] = Object.values(Spec);
+  hairColors: string[] = Object.values(HairColor);
+  eyeColors: string[] = Object.values(EyeColor);
+  birthStates: string[] = Object.values(BirthState);
 
-  constructor() { }
+  personForm = this.fb.group({
+    personFirstName: ['', Validators.required],
+    personLastName: ['', Validators.required],
+    personMiddleInitial: ['', Validators.maxLength],
+    personEmail: ['', Validators.email],
+    personDoDIDNumber: [
+      '',
+      [Validators.required, Validators.pattern('^[0-9]*$')],
+    ],
+    personSSN: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('(^\\d{3}-?\\d{2}-?\\d{4}$|^XXX-XX-XXXX$)'),
+        Validators.maxLength,
+      ],
+    ],
+    personBirthCountry: ['', Validators.required],
+    personBirthCity: ['', Validators.required],
+    personHeight: ['', Validators.required],
+    personBirthDate: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder, private apollo: Apollo) {}
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   ngOnInit(): void {
+    console.log('$testing');
   }
 
+  PersonSubmit(): void {
+    const SUBMIT_PERSON = gql`
+    mutation PersonCreateInput {
+      PersonCreateInput(
+        firstName: $firstName,
+        lastName: $lastName,
+        middleInitial: $middleInitial,
+        ssn: $ssn,
+        dodId: $dodId,
+        birthDate: $birthDate,
+        birthCity: $birthCity,
+        birthCountry: $birthCountry,
+        citizenshipId: $citizenshipId,
+        initialTraining: $initialTraining,
+        NDA: $NDA,
+        grade: $grade,
+        eyeColor: $eyeColor,
+        hairColor: $hairColor,
+        birthState: $birthState,
+        role: $role,
+        spec: $spec,
+        height: $height
+      )
+      {
+        id
+      }
+    }
+    `;
+    console.log(this.personForm.get(['personFirstName']));
+    this.apollo.mutate({
+      mutation: SUBMIT_PERSON,
+      variables: {
+        // firstName: this.personForm.value["personFirstName"],
+        // lastName: this.personForm.value["personLastName"],
+        // middleInitial: this.personForm.value["personMiddleInitial"],
+        // ssn: this.personForm.value["personSSN"],
+        // dodID: this.personForm.value["personDoDIDNumber"],
+        // birthDate: this.personForm.value["personBirthDate"],
+        // birthCity: this.personForm.value["personBirthCity"],
+        // birthCountry: this.personForm.value["personBirthCountry"],
+        // citizenshipId: this.personForm.value["personFirstName"],
+        // initialTraining: this.personForm.value["personFirstName"],
+        // NDA: this.personForm.value["personFirstName"],
+        // grade: this.personForm.value["personFirstName"],
+        // eyeColor: this.personForm.value["personFirstName"],
+        // haircolor: this.personForm.value["personFirstName"],
+        // birthState: this.personForm.value["personFirstName"],
+        // role: this.personForm.value["personFirstName"],
+        // spec: this.personForm.value["personFirstName"],
+        // height: this.personForm.value["personFirstName"]
+        firstName: 'Barry',
+        lastName: 'Benson',
+        middleInitial: 'B',
+        ssn: 420692022,
+        dodID: 1234567890,
+        birthDate: '02/11/2007',
+        birthCity: 'New York City',
+        birthCountry: 'US',
+        citizenshipId: 'Yes',
+        initialTraining: true,
+        NDA: false,
+        grade: 9,
+        eyeColor: 'BROWN',
+        haircolor: 'BROWN',
+        birthState: 'NY',
+        role: 'ADMIN',
+        spec: 'OFFICER',
+        height: 69,
+      }
+    });
+  }
 }
