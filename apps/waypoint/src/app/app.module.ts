11d13e8c10fcad10143a24e233dc4037e5fc80a4
brenden.mcgrew <brenden.mcgrew@us.af.mil>
2022-01-25 10:44:55 -0600

    Used material module

--- apps/waypoint/src/app/app.module.ts
+++ apps/waypoint/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Apollo } from 'apollo-angular';
@@ -8,17 +7,10 @@ import { GQLModule } from '@odst/gql';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CreatePersonComponent } from './create-person/create-person.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCardModule } from '@angular/material/card';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateOrgComponent } from './create-org/create-org.component';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -32,17 +24,9 @@ import { RouterModule } from '@angular/router';
     HttpClientModule,
     GQLModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatCardModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatCheckboxModule,
     FormsModule,
     ReactiveFormsModule,
-    MatIconModule,
+    MaterialModule
   ],
   providers: [Apollo],
   bootstrap: [AppComponent],
