97a6a6856d22d1bfda42f0ba582bed4bacc96b49
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-01-28 09:50:15 -0600

    update to create person test

--- apps/waypoint-e2e/src/integration/app.spec.ts
+++ apps/waypoint-e2e/src/integration/app.spec.ts
@@ -1,10 +1,10 @@
-import { getGreeting } from '../support/app.po';
+
 
 describe('waypoint', () => {
   beforeEach(() => cy.visit('/'));
 
   it.only('should create a new person', () => {
-    // Custom command example, see `../support/commands.ts` file
+
     cy.get('[formcontrolname="personFirstName"]').type('FirstName')
     cy.get('[formcontrolname="personLastName"]').type('LastName')
     cy.get('[formcontrolname="personDoDIDNumber"]').type('0123456789')
@@ -17,21 +17,18 @@ describe('waypoint', () => {
     cy.get('[formcontrolname="personEyeColor"]').click().get('mat-option').contains('HAZEL').click();
     cy.get('[formcontrolname="personSpec"]').click().get('mat-option').contains('OTHER').click();
     cy.get('[formcontrolname="personGrade"]').click().get('mat-option').contains('5').click();
-    cy.get('[formcontrolname="personOrg"]').click().get('mat-option').contains('552 ACNS').click();
+    cy.get('[formcontrolname="personOrg"]').click().get('mat-option').contains('Scorpion Developers').click();
     cy.get('[formcontrolname="personHeight"]').type('68')
     cy.get('[formcontrolname="personBirthDate"]').type('2000-01-01')
     cy.get('odst-create-person').find('button').contains('Submit').click()
 
-
-    //cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    //getGreeting().contains('Welcome to waypoint!');
   });
 
   it('should create a new org', () => {
+
     cy.get('[formcontrolname="orgName"]').type('552 ACNS')
     cy.get('[formcontrolname="orgTier"]').click().get('mat-option').contains('OTHER').click();
     cy.get('odst-create-org').find('button').click()
+    
   })
 });
