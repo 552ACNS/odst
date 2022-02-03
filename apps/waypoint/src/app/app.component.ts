4b7d2708d1b82afeb59bb6d8bfa9f65b541b79f8
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-02-03 13:45:29 -0600

    Org and Person tables created

--- apps/waypoint/src/app/app.component.ts
+++ apps/waypoint/src/app/app.component.ts
@@ -2,19 +2,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 import { OrgGQL, PersonGQL } from '@odst/types';
 import { Subscription } from 'rxjs';
-import { parse } from 'path/posix';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
  const GET_PERSONS = gql`
    query {
      findManyPersons {   
-       firstName   
+       firstName  
+       lastName 
        dodId
        ssn
      }
@@ -46,22 +39,8 @@ export class AppComponent implements OnInit, OnDestroy {
   birthCountry: "",  ssn: 0, birthDate: new Date(),
   citizenshipId: "", height: 0, initialTraining: true, 
   NDA: true, grade: 0, orgId: "", spec: "OTHER", role: "ADMIN"}];
-
-  querySubscription: Subscription;  
-dataToDisplay : PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
- tableProps = [
+  querySubscription: Subscription; 
+ tablePropsOrg = [
     {
       columnDef: 'id',
       header: '#',
@@ -72,11 +51,30 @@ dataToDisplay : PeriodicElement[] = [
     }, 
     {
       columnDef: 'aliases',
-      header: 'Weight',
+      header: 'Aliases',
     },
     {
       columnDef: 'OrgTier',
-      header: 'Symbol',
+      header: 'OrgTier',
+    },
+  ];
+
+  tablePropsPerson = [    
+    {
+      columnDef: 'firstName',
+      header: 'First Name',
+    }, 
+    {
+      columnDef: 'lastName',
+      header: 'Last Name',
+    },
+    {
+      columnDef: 'dodId',
+      header: 'DOD ID',
+    },
+    {
+      columnDef: 'ssn',
+      header: 'SSN',
     },
   ];
     
@@ -89,6 +87,15 @@ dataToDisplay : PeriodicElement[] = [
         this.loading = loading;
         this.orgs = data.findManyOrgs;
       });
+
+      this.querySubscription = this.apollo.watchQuery<any>({
+        query: GET_PERSONS
+      })
+        .valueChanges
+        .subscribe(({ data, loading }) => {
+          this.loading = loading;
+          this.persons = data.findManyPersons;
+        });
   }
 
   ngOnDestroy() {
