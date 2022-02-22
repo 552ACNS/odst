0576dc0dea8720aff27731aa03d86b3ee663b0b4
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-02-22 14:09:10 -0600

    Icons appear through *ngFor and updated formatting

--- apps/waypoint/src/app/navigation-bar/navigation-bar.component.ts
+++ apps/waypoint/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,6 +1,11 @@
 
     import { Component, Input } from '@angular/core';    
         
+    export interface NavProps {
+        path: string;
+        icon: string;
+        name: string;
+      }
     @Component({    
         selector:'odst-navigation-bar',    
         templateUrl:'./navigation-bar.component.html',    
@@ -9,5 +14,10 @@
 
 export class NavigationBarComponent    
 {
-    navLinks = [];
+    navLinks: NavProps[] = [
+        {path:'/login', icon:'home', name:'Home'},
+        {path: '/create-person', icon: 'build', name: 'Create'},
+        {path: '/create-org', icon: 'update', name: 'Update'},
+        {path: '/table-view', icon: 'visibility', name: 'View'}
+    ];
 }
