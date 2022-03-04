8daa1479f20e26ab311f336ce95c2c9f6c461323
Yuri Sim <edaraks@gmail.com>
2022-03-04 07:59:05 -0600

    change timeout to 10 seconds, contains to visible

--- apps/waypoint-e2e/src/integration/app.spec.ts
+++ apps/waypoint-e2e/src/integration/app.spec.ts
@@ -5,7 +5,7 @@ describe('waypoint', () => {
     cy.get('[formcontrolname="userPassword"]').type('admin');
     cy.get('odst-login').find('button').contains('Sign In').click();
     //TODO change to something that is directly correlated to a successful login
-    cy.get('#logo').contains('WP')
+    cy.get('#logo', { timeout: 10000 }).should('be.visible');
   });
 
   //need to run this test before create person
