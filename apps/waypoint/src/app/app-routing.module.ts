36928dd11dc4becec3af5786e061ad0a3b03c123
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-03-08 09:39:55 -0600

    refactored app routing module and added to nav routing module

--- apps/waypoint/src/app/app-routing.module.ts
+++ apps/waypoint/src/app/app-routing.module.ts
@@ -8,11 +8,11 @@ const routes: Routes = [
 
    { path: 'login', 
       loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'navigation-bar', component: NavigationBarComponent },
-  { path: 'create-person', 
-    loadChildren: () => import('./create-person/create-person.module').then(m => m.CreatePersonModule) },
-  { path: 'create-org', 
-    loadChildren: () => import('./create-org/create-org.module').then(m => m.CreateOrgModule) },
+  { path: 'home', component: NavigationBarComponent },
+  // { path: 'create-person', 
+  //   loadChildren: () => import('./create-person/create-person.module').then(m => m.CreatePersonModule) },
+  // { path: 'create-org', 
+  //   loadChildren: () => import('./create-org/create-org.module').then(m => m.CreateOrgModule) },
   { path: 'table-view', component: TableComponent },  
 ];
 
--- apps/waypoint/src/app/navigation-bar/navigation-bar-routing.module.ts
+++ apps/waypoint/src/app/navigation-bar/navigation-bar-routing.module.ts
@@ -8,12 +8,18 @@ import { TableComponent } from '../table/table.component';
 
 //TODO: clean up routes when routes needed for nav bar are final
 const routes: Routes = [
-  //{ path: 'login', component: LoginComponent },
-  { path: 'navigation-bar', component: NavigationBarComponent },
-  { path: 'create-person', component: CreatePersonComponent },
-  { path: 'create-org', component: CreateOrgComponent },  
-  { path: 'table-view', component: TableComponent },  
+  { path: '', component: NavigationBarComponent, children: [
+    { path: 'login', 
+      loadChildren: () => import('../login/login.module').then(m => m.LoginModule) },
+    { path: 'navigation-bar', component: NavigationBarComponent },
+    { path: 'create-person', 
+      loadChildren: () => import('../create-person/create-person.module').then(m => m.CreatePersonModule) },
+    { path: 'create-org', 
+      loadChildren: () => import('../create-org/create-org.module').then(m => m.CreateOrgModule) },
+    { path: 'table-view', component: TableComponent },  
+  ] }
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
