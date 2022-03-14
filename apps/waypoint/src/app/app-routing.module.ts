cf523f31a7ad399664e84c3f72f066dab7275c9d
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-03-14 13:58:04 -0500

    if url path empty, redirect to login. add more to this function later

--- apps/waypoint/src/app/app-routing.module.ts
+++ apps/waypoint/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ const routes: Routes = [
 
   { path: 'login', 
       loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  //TODO: add functionality to auto redirect to login if refresh token not found
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
   { path: 'home', component: NavigationBarComponent }, 
 ];
 @NgModule({
