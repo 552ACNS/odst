2e02719059f7e98fbbd90d2318c7997a1c33219a
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-03-16 13:55:18 -0500

    Delete test data and tests are in there own file

---
+++ apps/waypoint-e2e/src/integration/createOrg.spec.ts
@@ -0,0 +1,21 @@
+describe('waypoint', () => {
+    before(() => {
+        cy.visit('/login');
+        cy.get('[formcontrolname="userUsername"]').type('admin');
+        cy.get('[formcontrolname="userPassword"]').type('admin');
+        cy.get('odst-login').find('button').contains('Sign In').click();
+        cy.location('pathname').should('include', '/home');
+      }); 
+    it('should create a new org', () => {
+        cy.visit('/create-org');
+        cy.location('pathname').should('include', '/create-org');
+        cy.get('[formcontrolname="orgName"]').type('552 ACNS');
+        cy.get('[formcontrolname="orgTier"]')
+        .click()
+        .get('mat-option')
+        .contains('OTHER')
+        .click();
+        cy.get('#btnOrgSubmit').click();
+        cy.get('#orgCheck', { timeout: 10000 }).should('be.visible');
+    });
+});
