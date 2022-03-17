6d737422e91ad2c58bc6894ea73537c9ea94cf03
Yuri Sim <edaraks@gmail.com>
2022-03-17 14:30:06 -0500

    Created ods scaffolding

---
+++ apps/ods-api/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'ods-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/ods-api',
+};
