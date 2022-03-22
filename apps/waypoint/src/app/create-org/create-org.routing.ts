a5881fad578d75cfc55b82a4a8b2662c32cfcb05
eric.cater <eric.cater@us.af.mil>
2022-03-22 08:53:22 -0500

    fomatting

--- apps/waypoint/src/app/create-org/create-org.routing.ts
+++ apps/waypoint/src/app/create-org/create-org.routing.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
 import { CreateOrgComponent } from './create-org.component';
 
-export const createOrgRoutes: Routes = [{ path: '', component: CreateOrgComponent }];
+export const createOrgRoutes: Routes = [
+  { path: '', component: CreateOrgComponent },
+];
