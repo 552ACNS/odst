f05c683b98c321d663580399e1d400567e502108
Yuri Sim <edaraks@gmail.com>
2022-03-23 08:38:40 -0500

    setup material angular w/ tailwind w/ Goni
    (cherry picked from commit 1f4a237c4a8389950e5c8c56356c58f3d6a7c018)

--- apps/ods/src/app/app.module.ts
+++ apps/ods/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
+    MatButtonModule,
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
