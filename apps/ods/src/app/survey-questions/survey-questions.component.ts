10a12d3e1b75a6fd2d36689924063c4fa94ca951
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-03-23 09:06:16 -0500

    lalala

--- apps/ods/src/app/survey-questions/survey-questions.component.ts
+++ apps/ods/src/app/survey-questions/survey-questions.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'odst-survey-questions',
   templateUrl: './survey-questions.component.html',
   styleUrls: ['./survey-questions.component.scss']
 })
+
 export class SurveyQuestionsComponent {
 
-  constructor() { } 
+  constructor(private fb: FormBuilder,) { } 
+  form = this.fb.group({
+    personOrg: ['', Validators.required],
+    event: ['', Validators.required],
+  });
+  selected = 'option2';
 
+  // answers: string[] = [
+  //   this.form.value['personOrg'],
+  //   this.form.value['event'],
+  // ]
 }
