7b63c251f27e55eb1b8702a5facc7698cd048455
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-03-23 14:51:20 -0500

    fixed validator loading in problem

--- apps/ods/src/app/survey-questions/survey-questions.component.ts
+++ apps/ods/src/app/survey-questions/survey-questions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 
@@ -10,13 +10,26 @@ import { FormBuilder, Validators } from '@angular/forms';
 
 export class SurveyQuestionsComponent {
 
-  constructor(private fb: FormBuilder,) { } 
+  constructor(private fb: FormBuilder) { } 
   form = this.fb.group({
     personOrg: ['', Validators.required],
     event: ['', Validators.required],
+    violatorSpec: [Validators.required],
+    violatorOtherSpec: [''],
+    CC: ['', Validators.required],
+    personSpec: [Validators.required],
+    personOtherSpec: [''],
+    impact: ['', Validators.required],
+    outsideRouting: [Validators.required]
   });
   selected = 'option2';
-
+  
+  submit(){
+    return;
+  }
+  back(){
+    return;
+  }
   // answers: string[] = [
   //   this.form.value['personOrg'],
   //   this.form.value['event'],
