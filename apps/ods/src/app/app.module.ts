c7c70e2ea064b35dc1d4f44e287475470d41f892
Yuri Sim <edaraks@gmail.com>
2022-03-23 08:41:59 -0500

    Revert "setup material angular w/ tailwind w/ Goni"
    This reverts commit 1f4a237c4a8389950e5c8c56356c58f3d6a7c018.

--- apps/ods/src/app/app.module.ts
+++ apps/ods/src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NxWelcomeComponent],
   imports: [
-    MatButtonModule,
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
-    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
