b5014bd43bf627ddd485c10404043f7e4c51478d
eric.cater <eric.cater@us.af.mil>
2022-03-24 13:40:17 -0500

    user input types

--- libs/types/ods/src/input/user.create.input.ts
+++ libs/types/ods/src/input/user.create.input.ts
@@ -1,5 +1,24 @@
-import { InputType } from '@nestjs/graphql';
-import { Prisma } from '.prisma/ods/client';
+import { InputType, Field } from '@nestjs/graphql';
+import { Prisma, Role } from '.prisma/ods/client';
+
+@InputType()
+export class UserCreateInput implements Prisma.UserCreateInput {
+  email: string;
+  password: string;
+
+  enabled?: boolean;
+
+  @Field(() => Role)
+  roles: Role;
+
+  //TODO org
+
+  //TODO refreshtoken
+  // @Field(() => RefreshTokenCreateNestedManyWithoutUserInput)
+  // refreshTokens?: Prisma.RefreshTokenCreateNestedManyWithoutUserInput;
+}
+
+
 
 @InputType()
 export class UserCreateNestedManyWithoutOrgsInput
---
+++ libs/types/ods/src/input/user.update.input.ts
@@ -0,0 +1,15 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { Prisma, Role } from '.prisma/ods/client';
+
+@InputType()
+export class UserUpdateInput implements Prisma.UserUpdateInput {
+  email?: string;
+  password?: string;
+  enabled?: boolean;
+
+  @Field(() => Role)
+  roles: Role;
+
+  //TODO refreshToken, org
+
+}
