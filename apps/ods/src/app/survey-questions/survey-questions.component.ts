174c0fe59dad686f1778a1bfb688333ca66024b7
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-03-24 08:41:43 -0500

    Reformated select to be *ngFor, validators now working

--- apps/ods/src/app/survey-questions/survey-questions.component.ts
+++ apps/ods/src/app/survey-questions/survey-questions.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'odst-survey-questions',
   templateUrl: './survey-questions.component.html',
@@ -10,20 +9,24 @@ import { FormBuilder, Validators } from '@angular/forms';
 
 export class SurveyQuestionsComponent {
 
+  orgs: string[] = ['552 ACNS', 'Astolfo Gang', 'Random'];
+  CCs: string[] = ['Matos, Emmanuel Lt. Col.', 'God Emperer Astolfo', 'MaGoo'];
   constructor(private fb: FormBuilder) { } 
-  form = this.fb.group({
-    personOrg: ['', Validators.required],
-    event: ['', Validators.required],
-    violatorSpec: [Validators.required],
-    violatorOtherSpec: [''],
-    CC: ['', Validators.required],
-    personSpec: [Validators.required],
-    personOtherSpec: [''],
-    impact: ['', Validators.required],
-    outsideRouting: [Validators.required]
-  });
-  selected = 'option2';
   
+  //validators didnt need to be inside the formControlName and caused it to break, so i removed
+  //them. Also, all form controls need to be specificed in their or else they all will not
+  //load properly.
+  form = this.fb.group({
+    personOrg: [],
+    event: [],
+    violatorSpec: [],
+    violatorOtherSpec: [],
+    CC: [],
+    personSpec: [],
+    personOtherSpec: [],
+    impact: [],
+    outsideRouting: []
+  });
   submit(){
     return;
   }
