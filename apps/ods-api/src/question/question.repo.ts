c66f7d194ae23abc9df4b9670645fcc717e8a17d
Yuri Sim <edaraks@gmail.com>
2022-03-24 08:10:38 -0500

    question repo

--- apps/ods-api/src/question/question.repo.ts
+++ apps/ods-api/src/question/question.repo.ts
@@ -2,7 +2,7 @@ import { QuestionCreateInput } from './dto/create-question.input';
 import { Question } from '.prisma/ods/client';
 import { cloneDeep } from '@apollo/client/utilities';
 
-export const mockQuestionInputs: QuestionCreateInput[] = [
+export const mockQuestionCreateInputs: QuestionCreateInput[] = [
   {
     id: '1',
     prompt: 'Question 1',
@@ -32,8 +32,10 @@ export const mockQuestionInputs: QuestionCreateInput[] = [
   },
 ];
 
-// Need to clone deep because of reference types
-export const mockQuestions: Question[] = cloneDeep(mockQuestionInputs).map(
+// Need to clone deep because of reference types. We delete the survey field
+// from the mockQuestionInputs because the Question type does not have a survey
+// field
+export const mockQuestions: Question[] = cloneDeep(mockQuestionCreateInputs).map(
   (questionInputs) => {
     delete questionInputs.survey;
     return questionInputs as Question;
--- apps/ods-api/src/question/question.service.spec.ts
+++ apps/ods-api/src/question/question.service.spec.ts
@@ -1,8 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuestionService } from './question.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { mockQuestions, mockQuestionInputs } from './question.repo';
-import { QuestionCreateInput } from './dto/create-question.input';
+import { mockQuestions, mockQuestionCreateInputs } from './question.repo';
 
 //Mock the db
 const prismaDB = {
@@ -36,14 +35,18 @@ describe('QuestionService', () => {
   });
 
   it('should create new question ', async () => {
-    const inputs: QuestionCreateInput = mockQuestionInputs[0]
+    const result = service.create(mockQuestionCreateInputs[0]);
     const expected = mockQuestions[0];
 
-    expect(service.create(inputs)).resolves.toEqual(expected);
+    expect(result).resolves.toEqual(expected);
   });
 
-  it('should find all questions in a survey', async () => {
-    expect(service.findQuestionsInSurvey('survey1')).resolves.toEqual(
+  it('should find all questions from a specific survey', async () => {
+    const result = service.findQuestionsInSurvey('survey1')
+
+    const expected = mockQuestionCreateInputs.
+
+    expect(result).resolves.toEqual(
       mockQuestions
     );
   });
