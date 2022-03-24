c575d557c81f01b491349dae8bd2f2801e48f30a
pmeskridge <eskridge_pk@cox.net>
2022-03-24 08:56:29 -0500

    Removed unecessary imports, routed to disclaimer

--- apps/ods/src/app/app.routing.ts
+++ apps/ods/src/app/app.routing.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
 
   { path: 'disclaimer', 
       loadChildren: () => import('./disclaimer/disclaimer.module').then(m => m.DisclaimerModule) },
+      { path: '', redirectTo: '/disclaimer', pathMatch: 'full' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- apps/ods/src/app/disclaimer/disclaimer.module.ts
+++ apps/ods/src/app/disclaimer/disclaimer.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DisclaimerComponent } from './disclaimer.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { DisclaimerRoutes } from './disclaimer.routing';
 
