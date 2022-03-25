ca28b8c454654a756587f90dbd1e021d7282c835
eric.cater <eric.cater@us.af.mil>
2022-03-25 15:29:27 -0500

    seed db with org, user, survey, survey questions

--- apps/ods-api/prisma-seed.ts
+++ apps/ods-api/prisma-seed.ts
@@ -1,87 +1,93 @@
-// import { PrismaClient } from '.prisma/ods/client';
-// import { PrismaClientKnownRequestError } from '.prisma/ods/client/runtime';
-// import { hash } from 'bcrypt';
+import { PrismaClient } from '.prisma/ods/client';
+import { PrismaClientKnownRequestError } from '.prisma/ods/client/runtime';
+import { hash } from 'bcrypt';
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// async function main() {
-//   const org = await prisma.org.upsert({
-//     where: {
-//       name: 'Scorpion Developers',
-//     },
-//     update: {
-//       orgTier: 'OTHER',
-//     },
-//     create: {
-//       name: 'Scorpion Developers',
-//       orgTier: 'OTHER',
-//     },
-//   });
+async function main() {
+  //#region org
+  const org = await prisma.org.upsert({
+    where: {
+      name: 'Scorpion Developers',
+    },
+    update: {
+      orgTier: 'OTHER',
+    },
+    create: {
+      name: 'Scorpion Developers',
+      orgTier: 'OTHER',
+    },
+  });
+  //#endregion
 
-//   const person = await prisma.person.upsert({
-//     where: { dodId: 100000000 },
-//     update: { org: { connect: { name: org.name } } },
-//     create: {
-//       dodId: 100000000,
-//       ssn: 100000000,
-//       email: 'dev_admin_account@dev_admin_account',
-//       hairColor: 'BROWN',
-//       birthState: 'AK',
-//       eyeColor: 'BROWN',
-//       spec: 'OTHER',
-//       role: 'DEV',
-//       firstName: 'dev_admin_account',
-//       lastName: 'dev_admin_account',
-//       birthDate: new Date('1/1/1990'),
-//       org: { connect: { name: org.name } },
-//       grade: 1,
-//       NDA: false,
-//       initialTraining: false,
-//       citizenshipId: 'dev_admin_account',
-//       height: 1,
-//       birthCountry: 'dev_admin_account',
-//       birthCity: 'dev_admin_account',
-//     },
-//   });
-//   try {
-//     await prisma.refreshToken.deleteMany({
-//       where: { user: { username: 'admin' } },
-//     });
-//     await prisma.user.delete({ where: { username: 'admin' } });
-//   } catch (e) {
-//     //delete can fail if no entities are found. Ignore that
-//     if (!(e instanceof PrismaClientKnownRequestError)) {
-//       throw e;
-//     }
-//   }
+  //#region user admin
+  //delete existing user
+  try {
+    await prisma.refreshToken.deleteMany({
+      where: { user: { email: 'admin@admin.com' } },
+    });
+    await prisma.user.delete({ where: { email: 'admin@admin.com' } });
+  } catch (e) {
+    //delete can fail if no entities are found. Ignore that
+    if (!(e instanceof PrismaClientKnownRequestError)) {
+      throw e;
+    }
+  }
 
-//   if (
-//     process.env.NODE_ENV !== 'PRODUCTION' &&
-//     process.env.NX_DEV_ACCOUNT_PASSWORD
-//   ) {
-//     const pw = await hash(process.env.NX_DEV_ACCOUNT_PASSWORD, 10);
+  if (
+    process.env.NODE_ENV !== 'PRODUCTION' &&
+    process.env.NX_DEV_ACCOUNT_PASSWORD
+  ) {
+    const pw = await hash(process.env.NX_DEV_ACCOUNT_PASSWORD, 10);
 
-//     const user = await prisma.user.create({
-//       data: {
-//         username: 'admin',
-//         password: pw,
-//         person: { connect: { dodId: person.dodId } },
-//         enabled: true,
-//       },
-//     });
-//     console.log({ org, person, user });
-//   } else {
-//     console.log({ org, person });
-//   }
-// }
+    const user = await prisma.user.create({
+      data: {
+        email: 'admin@admin.com',
+        password: pw,
+        orgs: { connect: { name: 'Scorpion Developers' } },
+        roles: 'ADMIN',
+      },
+    });
+    console.log({ user });
+  }
+  //#endregion user admin
 
-// main()
-//   .catch((e) => {
-//     console.error(e);
-//     process.exit(1);
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
+  //#region survey
+  //delete existing test surveys
+  try {
+    await prisma.survey.deleteMany({
+      where: { orgs: { every: { name: 'Scorpion Developers' } } },
+    });
+  } catch (e) {
+    //delete can fail if no entities are found. Ignore that
+    if (!(e instanceof PrismaClientKnownRequestError)) {
+      throw e;
+    }
+  }
 
-console.log("Please update this file when you want to seed something bruh")
+  const survey = await prisma.survey.create({
+    data: {
+      orgs: { connect: { id: 'Scorpion Developers' } },
+      questions: {
+        createMany: {
+          data: [
+            { prompt: 'What is the meaning of life?' },
+            { prompt: 'Am I alive?' },
+          ],
+        },
+      },
+    },
+  });
+  //#endregion survey
+
+  console.log({ org, survey });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
