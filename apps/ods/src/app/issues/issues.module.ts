536bbc381e9a38e110f01d3f842f956cbbf48fd2
Yuri Sim <edaraks@gmail.com>
2022-03-25 08:55:59 -0500

    removed issues bar component

--- apps/ods/src/app/issues/issues.module.ts
+++ apps/ods/src/app/issues/issues.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { IssuesComponent } from './issues.component';
 import { IssuesRoutes } from './issues.routing';
-import { IssuesBarComponent } from '../issues-bar/issues-bar.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [IssuesComponent, IssuesBarComponent],
+  declarations: [IssuesComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(IssuesRoutes),
