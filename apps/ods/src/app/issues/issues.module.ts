fb8634fa15a93137548e39c01a288ca830d92544
Yuri Sim <edaraks@gmail.com>
2022-03-28 10:19:37 -0500

    removed unused import

--- apps/ods/src/app/issues/issues.module.ts
+++ apps/ods/src/app/issues/issues.module.ts
@@ -7,7 +7,6 @@ import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [IssuesComponent],
