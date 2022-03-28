131a2d12776b9827adc7f6be913ac934183f5c0b
Yuri Sim <edaraks@gmail.com>
2022-03-28 09:22:39 -0500

    fixing formats

--- apps/ods/src/app/issues/issues.component.spec.ts
+++ apps/ods/src/app/issues/issues.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { IssuesComponent } from './issues.component';
 import { IssuesModule } from './issues.module';
@@ -10,7 +11,7 @@ describe('IssuesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ IssuesComponent ],
-      imports: [IssuesModule]
+      imports: [IssuesModule, BrowserAnimationsModule], 
     })
     .compileComponents();
   });
--- apps/ods/src/app/issues/issues.module.ts
+++ apps/ods/src/app/issues/issues.module.ts
@@ -6,6 +6,8 @@ import { IssuesRoutes } from './issues.routing';
 import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [IssuesComponent],
@@ -14,6 +16,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     RouterModule.forChild(IssuesRoutes),
     MatPaginatorModule,
     MatCardModule,
+    MatInputModule,
     //temporary
     MatFormFieldModule,
   ],
