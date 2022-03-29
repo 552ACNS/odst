0c31dd8d404284c593bc422189e8994a45ab47cd
eric.cater <eric.cater@us.af.mil>
2022-03-29 12:50:51 -0500

    add gql to backend, update angular.json

--- apps/ods-api/src/app/app.module.ts
+++ apps/ods-api/src/app/app.module.ts
@@ -1,11 +1,36 @@
 import { Module } from '@nestjs/common';
+import {
+  Role,
+  // Spec,
+  OrgTier,
+} from '.prisma/ods/client';
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageDisabled,
+} from 'apollo-server-core';
+import { join } from 'path';
+import { GraphQLModule, registerEnumType } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// Register enum types here, if they are used in multiple places, make sure that they are registered
+// only once and that the resource module that is imported first is the one that registers them
+registerEnumType(Role, { name: 'Role' });
+// registerEnumType(Spec, { name: 'Spec' });
+registerEnumType(OrgTier, { name: 'OrgTier' });
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'apps/ods-api/schema.graphql'),
+      playground: false,
+      introspection: process.env.NODE_ENV !== 'production',
+      driver: ApolloDriver,
+      plugins: [
+        process.env.NODE_ENV === 'production'
+          ? ApolloServerPluginLandingPageDisabled()
+          : ApolloServerPluginLandingPageLocalDefault(),
+      ],
+    }),
+  ],
 })
 export class AppModule {}
