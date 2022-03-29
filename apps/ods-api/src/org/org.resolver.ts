25b64f335e393d817238d9dffd5c4f0ea729e8d6
eric.cater <eric.cater@us.af.mil>
2022-03-29 11:53:47 -0500

    adding org backend

---
+++ apps/ods-api/src/org/org.resolver.ts
@@ -0,0 +1,70 @@
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
+import { OrgService } from './org.service';
+import {
+  OrgGQL,
+  OrgCreateInput,
+  OrgUpdateInput,
+  OrgWhereUniqueInput,
+} from '@odst/types/ods';
+//import { AccessTokenAuthGuard } from '../auth/guards/accessToken.authGuard';
+// import { UseGuards } from '@nestjs/common';
+
+@Resolver(() => OrgGQL)
+export class OrgResolver {
+  constructor(private readonly orgService: OrgService) {}
+
+  @Query(() => [OrgGQL], { name: 'findManyOrgs' })
+  // @UseGuards(AccessTokenAuthGuard)
+  async findMany(): Promise<OrgGQL[]> {
+    // return this.orgService.findMany();
+    return this.orgService.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+    });
+  }
+
+  @Query(() => [OrgGQL], { name: 'getSubOrgs' })
+  // @UseGuards(AccessTokenAuthGuard)
+  async getSubOrgs(
+    @Args('orgWhereUniqueInput')
+    orgWhereUniqueInput: OrgWhereUniqueInput
+  ): Promise<OrgGQL[]> {
+    return this.orgService.getSubOrgs(orgWhereUniqueInput);
+  }
+
+  @Query(() => OrgGQL, { name: 'findUniqueOrg' })
+  // @UseGuards(AccessTokenAuthGuard)
+  async findUnique(
+    @Args('orgWhereUniqueInput')
+    orgWhereUniqueInput: OrgWhereUniqueInput
+  ): Promise<OrgGQL | null> {
+    return this.orgService.findUnique(orgWhereUniqueInput);
+  }
+
+  @Mutation(() => OrgGQL, { name: 'createOrg' })
+  // @UseGuards(AccessTokenAuthGuard)
+  create(@Args('orgCreateInput') orgCreateInput: OrgCreateInput) {
+    return this.orgService.create(orgCreateInput);
+  }
+
+  @Mutation(() => OrgGQL, { name: 'updateOrg' })
+  // @UseGuards(AccessTokenAuthGuard)
+  async update(
+    @Args('OrgWhereUniqueInput')
+    orgWhereUniqueInput: OrgWhereUniqueInput,
+    @Args('OrgUpdateInput')
+    orgUpdateInput: OrgUpdateInput
+  ): Promise<OrgGQL> {
+    return this.orgService.update(orgWhereUniqueInput, orgUpdateInput);
+  }
+
+  @Mutation(() => OrgGQL, { name: 'deleteOrg' })
+  // @UseGuards(AccessTokenAuthGuard)
+  async delete(
+    @Args('orgWhereUniqueInput')
+    orgWhereUniqueInput: OrgWhereUniqueInput
+  ): Promise<{ deleted: boolean }> {
+    return this.orgService.delete(orgWhereUniqueInput);
+  }
+}
