226b31fa2a319012b7feb5f7c02af1f832c30ce7
pmeskridge <eskridge_pk@cox.net>
2022-03-31 12:43:12 -0500

    Created login component files.

--- apps/ods/src/app/app.module.ts
+++ apps/ods/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app.routing';
 import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
+import { Apollo } from 'apollo-angular';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [Apollo, { provide: 'environment', useValue: environment }],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
---
+++ apps/ods/src/app/login/login.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Apollo } from 'apollo-angular';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private apollo: Apollo, private router: Router) {}
+  submitLogin(username: string, password: string): void {}
+
+  
+}
