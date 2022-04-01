a87876a478e1e2d4b589a65cfccc015ceec30ed0
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-04-01 13:04:28 -0500

    fixing resolver tests

--- libs/types/ods/src/input/survey.update.input.ts
+++ libs/types/ods/src/input/survey.update.input.ts
@@ -1,13 +1,32 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { Prisma } from '.prisma/ods/client';
-import { SurveyCreateInput } from './survey.create.input';
 import { QuestionUpdateManyWithoutSurveysInput } from './question.unique.input';
+import { SurveyResponseGQL } from '../entity/surveyResponse.entity';
 
 @InputType()
-export class SurveyUpdateInput
-  // extends SurveyCreateInput
-  implements Prisma.SurveyUpdateInput {
+// extends SurveyCreateInput
+export class SurveyUpdateInput implements Prisma.SurveyUpdateInput {
+  @Field(() => QuestionUpdateManyWithoutSurveysInput)
+  questions?: Prisma.QuestionUpdateManyWithoutSurveysInput;
 
-    @Field(() => QuestionUpdateManyWithoutSurveysInput)
-    questions?: Prisma.QuestionUpdateManyWithoutSurveysInput;
-  }
+  @Field(() => SurveyResponseUpdateManyWithoutSurveyInput)
+  surveyResponses?: Prisma.SurveyResponseUpdateManyWithoutSurveyInput;
+}
+
+@InputType()
+export class SurveyResponseUpdateManyWithoutSurveyInput
+  implements Prisma.SurveyResponseUpdateManyWithoutSurveyInput
+{
+  @Field(() => SurveyResponseCreateManySurveyInputEnvelope)
+  createMany?: Prisma.SurveyResponseCreateManySurveyInputEnvelope;
+}
+
+@InputType()
+export class SurveyResponseCreateManySurveyInputEnvelope
+  implements Prisma.SurveyResponseCreateManySurveyInputEnvelope
+{
+  @Field(() => [SurveyResponseGQL], { nullable: true })
+  data: Prisma.Enumerable<Prisma.SurveyResponseCreateManySurveyInput>;
+  skipDuplicates?: boolean;
+}
+
