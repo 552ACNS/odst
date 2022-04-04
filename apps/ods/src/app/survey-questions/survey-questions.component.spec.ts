956df1276eea9eb3a9c80f668c17ee851db3b2fb
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-04-04 09:27:46 -0500

    Added more stuff to test

--- apps/ods/src/app/survey-questions/survey-questions.component.spec.ts
+++ apps/ods/src/app/survey-questions/survey-questions.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { SurveyQuestionsComponent } from './survey-questions.component';
 import { ApolloTestingModule } from 'apollo-angular/testing';
 import { MatCardModule } from '@angular/material/card';
@@ -9,6 +9,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SurveyQuestionsComponent', () => {
   let component: SurveyQuestionsComponent;
@@ -19,6 +21,7 @@ describe('SurveyQuestionsComponent', () => {
       declarations: [ SurveyQuestionsComponent ],
       imports: [
         ReactiveFormsModule, 
+        FormsModule,
         ApolloTestingModule,
         MatCardModule,
         MatCheckboxModule,
@@ -26,17 +29,19 @@ describe('SurveyQuestionsComponent', () => {
         MatIconModule,
         MatButtonModule,
         MatSelectModule,
-        MatRadioModule
+        MatRadioModule,
+        BrowserModule,
+        BrowserAnimationsModule,
       ]
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SurveyQuestionsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(SurveyQuestionsComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
   it('should create', () => {
     expect(component).toBeTruthy();
