9fd728ae6f763a12cae455160d95c25d66a47bf2
Yuri Sim <edaraks@gmail.com>
2022-04-04 00:54:30 -0500

    update the login service

--- apps/ods/src/app/login/login.service.spec.ts
+++ apps/ods/src/app/login/login.service.spec.ts
@@ -1,16 +1,24 @@
-import { TestBed } from '@angular/core/testing';
-
+import { HttpModule } from '@nestjs/axios';
+import { Test, TestingModule } from '@nestjs/testing';
 import { LoginService } from './login.service';
 
 describe('LoginService', () => {
   let service: LoginService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LoginService);
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LoginService],
+      imports: [HttpModule],
+    }).compile();
+    // getting service module from main module
+    service = module.get<LoginService>(LoginService);
+  });
+  // now checking if service is available
+  it('ApiService - should be defined', () => {
+    expect(service).toBeDefined();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeDefined();
   });
 });
