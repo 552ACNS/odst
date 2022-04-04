af52776e917d060eebaae8be15b72e139219126b
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-04-04 09:25:36 -0500

    Added mat inports to test

--- apps/ods/src/app/survey-questions/survey-questions.component.spec.ts
+++ apps/ods/src/app/survey-questions/survey-questions.component.spec.ts
@@ -2,6 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SurveyQuestionsComponent } from './survey-questions.component';
 import { ApolloTestingModule } from 'apollo-angular/testing';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
 
 describe('SurveyQuestionsComponent', () => {
   let component: SurveyQuestionsComponent;
@@ -10,7 +17,17 @@ describe('SurveyQuestionsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ SurveyQuestionsComponent ],
-      imports: [ReactiveFormsModule, ApolloTestingModule]
+      imports: [
+        ReactiveFormsModule, 
+        ApolloTestingModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatRadioModule
+      ]
     })
     .compileComponents();
   });
