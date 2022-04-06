5a50e3570a1f54b9f54db180ea0bcf5788131342
Yuri Sim <edaraks@gmail.com>
2022-04-06 07:50:41 -0500

    fix tests

--- apps/ods/src/app/responses/responses.component.spec.ts
+++ apps/ods/src/app/responses/responses.component.spec.ts
@@ -1,25 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
+import {
+  ApolloTestingController,
+  ApolloTestingModule,
+} from 'apollo-angular/testing';
 
 import { ResponsesComponent } from './responses.component';
 import { ResponsesModule } from './responses.module';
 import { ResponsesService } from './responses.service';
 
 describe('ResponsesComponent', () => {
-  let fixture: ComponentFixture<ResponsesComponent>;
+  // let fixture: ComponentFixture<ResponsesComponent>;
+  let controller: ApolloTestingController;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [ResponsesComponent],
       providers: [ResponsesService],
-      imports: [ResponsesModule, ApolloTestingModule, BrowserAnimationsModule, ReactiveFormsModule],
-    }).compileComponents()
+      imports: [
+        ResponsesModule,
+        ApolloTestingModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+      ],
+    }).compileComponents();
 
-
-    fixture = TestBed.createComponent(ResponsesComponent);
-    fixture.detectChanges();
+    controller = TestBed.inject(ApolloTestingController);
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -27,15 +35,19 @@ describe('ResponsesComponent', () => {
     // expect(component).toBeTruthy();
   });
 
+  afterEach(() => {
+    controller.verify();
+  });
+
   // describe('load response ids', () => {
   //   it('should return response ids', (done) => {
   //     const mockResponseIDs  = ['a uuid', 'another uuid', 'yet another uuid'];
-  
+
   //     (service.getResponseIDsByStatus(true)).subscribe(serverResponse => {
   //       expect(serverResponse).toEqual(mockResponseIDs);
   //       done();
   //     });
-  
+
   //     // const req = controller.expectOne(GetIssuesByStatusDocument);
 
   //     // expect(req.operation.operationName).toBe('getIssuesByStatus');
