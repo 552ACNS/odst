8d0147a51a23b6afbbbbed2fa480654072045314
brenden.mcgrew <brenden.mcgrew@us.af.mil>
2022-04-07 09:24:26 -0500

    Added ngfor to create repeating elements & fixed styling issues.

--- apps/ods/src/app/dashboard/dashboard.component.ts
+++ apps/ods/src/app/dashboard/dashboard.component.ts
@@ -1,33 +1,60 @@
-import {Component, OnInit} from '@angular/core';
-import {DashboardService} from "./dashboard.service";
-import {SurveyResponseGql} from "../../graphql-generated";
-
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './dashboard.service';
+import { SurveyResponseGql } from '../../graphql-generated';
+import { NotificationCard } from './notification-card';
 
 @Component({
   selector: 'odst-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-
-  constructor(private dashboardService: DashboardService) {
-
-  }
+  constructor(private dashboardService: DashboardService) {}
 
   responses: SurveyResponseGql[];
   unresolvedReports: number;
   overdueReports: number;
   resolvedReports: number;
+  CardSpecs: NotificationCard[];
 
   ngOnInit() {
     this.dashboardService.GetResponseCount().subscribe((data) => {
-      console.log(data)
       this.responses = data.data.findManySurveyResponses;
-      this.unresolvedReports = this.responses.filter(issue => issue.resolution == null).length;
-      this.resolvedReports = this.responses.filter(issue => issue.resolution != null).length;
-      this.overdueReports = this.responses.filter(issue => this.dateIsMoreThan30DaysAgo(new Date(issue.openedDate))).length;
-    })
+
+      this.CardSpecs = [
+        {
+          Title: 'Unresolved',
+          NumberStyle:
+            'text-7xl sm:text-8xl font-bold tracking-tight leading-none text-blue-500',
+          CountOf: this.responses.filter((issue) => issue.resolution == null)
+            .length,
+          Suffix: 'Unresolved Reports',
+          SuffixStyle:
+            'text-lg font-medium text-blue-600 dark:text-blue-500 text-center',
+        },
+        {
+          Title: 'Overdue',
+          NumberStyle:
+            'text-7xl sm:text-8xl font-bold tracking-tight leading-none text-red-500',
+          CountOf: this.responses.filter((issue) =>
+            this.dateIsMoreThan30DaysAgo(new Date(issue.openedDate))
+          ).length,
+          Suffix: 'Overdue Reports',
+          SuffixStyle:
+            'text-lg font-medium text-red-600 dark:text-red-500 text-center',
+        },
+        {
+          Title: 'Resolved',
+          NumberStyle:
+            'text-7xl sm:text-8xl font-bold tracking-tight leading-none text-green-500',
+          CountOf: this.responses.filter((issue) => issue.resolution != null)
+            .length,
+          Suffix: 'Resolved Reports',
+          SuffixStyle:
+            'text-lg font-medium text-green-600 dark:text-green-500 text-center',
+        },
+      ];
+    });
   }
 
   dateIsMoreThan30DaysAgo(responseDate: Date) {
---
+++ apps/ods/src/app/dashboard/notification-card.ts
@@ -0,0 +1,8 @@
+export interface NotificationCard {
+  Title: string;
+  NumberStyle: string;
+  CountOf: number;
+  Suffix: string;
+  SuffixStyle: string;
+  CardRoute?: string;
+}
