3c9c9ad6eabf1049dabb24d4bd255f4899b77f4e
sayntonge <83591325+sayntonge@users.noreply.github.com>
2022-04-13 09:58:21 -0500

    cypress test works

--- apps/ods-e2e/src/integration/submitSurvey.spec.ts
+++ apps/ods-e2e/src/integration/submitSurvey.spec.ts
@@ -3,6 +3,7 @@ describe('ods', () => {
     cy.intercept('POST', '**/graphql').as('graphql');
   });
   before(() => {
+    //TODO: Implement when login is a thing.
     //   cy.visit('/login');
     //   cy.get('[formcontrolname="userUsername"]').type('admin');
     //   cy.get('[formcontrolname="userPassword"]').type('admin');
@@ -14,41 +15,24 @@ describe('ods', () => {
     cy.location('pathname').should('include', '/disclaimer');
     cy.get('odst-disclaimer').find('button').contains('Accept').click();
     cy.location('pathname').should('include', '/survey');
-
-    // cy.get('[formcontrolname="eventOrg"]').select('#mat-option-1')
-
-    // select the write option and press enter
-    // cy.get('mat-select[formControlName=eventOrg]').click().first().click();
-    // cy.get('[formcontrolname="eventOrg"]').focus().click();
-
     cy.contains('span', 'Organization')
       .click()
       .wait('@graphql')
       .focused()
       .click({ force: true })
       .type('{enter}');
-
-    // cy.contains('span', 'Organization').click();
-
-    // select the first option
-    // cy.get('mat-option').first().click();
-
-    // cy.type('{enter}');
-
-    // cy.contains('552 ACNS').click({ force: true });
-
-    // cy.get('mat-option')
-
-    // cy.get('[formcontrolname="event"]').type('e2e Test');
-    // cy.get('#mat-radio-3').click();
-    // cy.get('[formcontrolname="CC')
-    //   .click()
-    //   .get('mat-option')
-    //   .contains('Matos, Emmanuel Lt. Col.')
-    //   .click();
-    // cy.get('#mat-radio-8').click();
-    // cy.get('[formcontrolname="impact"]').type('it made me cry');
-    // cy.get('#btnSubmit').click();
-    // cy.get('#submitCheck', { timeout: 10000 }).should('be.visible');
+    cy.get('[formcontrolname="event"]').type('e2e Test');
+    cy.get('#mat-radio-3').click();
+    cy.get('[formcontrolname="CC')
+      .click()
+      //TODO: put this back when CC query is implemented
+      //.wait('@graphql')
+      .focused()
+      .click({ force: true })
+      .type('{enter}');
+    cy.get('#mat-radio-8').click();
+    cy.get('[formcontrolname="impact"]').type('it made me cry');
+    cy.get('#btnSubmit').click();
+    cy.get('#submitCheck', { timeout: 10000 }).should('be.visible');
   });
 });
