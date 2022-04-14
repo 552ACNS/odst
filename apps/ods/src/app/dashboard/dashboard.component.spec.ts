daf3abeb7f0528fa0f1b09fec7a6c3ff29687b33
eric.cater <eric.cater@us.af.mil>
2022-04-14 14:39:05 -0500

    nx format

--- apps/ods/src/app/dashboard/dashboard.component.spec.ts
+++ apps/ods/src/app/dashboard/dashboard.component.spec.ts
@@ -1,8 +1,8 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApolloTestingModule } from 'apollo-angular/testing';
-import {DashboardComponent} from './dashboard.component';
-import {DashboardModule} from "./dashboard.module";
+import { DashboardComponent } from './dashboard.component';
+import { DashboardModule } from './dashboard.module';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
