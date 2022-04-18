c93a0af9f5fef6b0dc2930e0a6dbb947a2cbdd4b
Yuri Sim <edaraks@gmail.com>
2022-04-18 15:43:44 -0500

    merged prisma-seed into prisma-seed-dev and deleted old prisma seed and renamed the dev to prisma-seed

--- apps/ods-api/prisma-seed.ts
+++ apps/ods-api/prisma-seed.ts
@@ -5,6 +5,9 @@ import { hash } from 'bcrypt';
 
 const prisma = new PrismaClient();
 
+const orgName = '552 ACNS';
+const CCEmail = 'john.doe@us.af.mil';
+
 async function main() {
   //#region org
   await prisma.org.upsert({
@@ -17,6 +20,42 @@ async function main() {
     create: {
       name: 'Scorpion Developers',
       orgTier: 'OTHER',
+    },
+  });
+  await prisma.org.upsert({
+    where: {
+      name: orgName,
+    },
+    update: {
+      orgTier: 'SQUADRON',
+    },
+    create: {
+      name: orgName,
+      orgTier: 'SQUADRON',
+    },
+  });
+  await prisma.org.upsert({
+    where: {
+      name: '552 MXS',
+    },
+    update: {
+      orgTier: 'SQUADRON',
+    },
+    create: {
+      name: '552 MXS',
+      orgTier: 'SQUADRON',
+    },
+  });
+  await prisma.org.upsert({
+    where: {
+      name: '752 OSS',
+    },
+    update: {
+      orgTier: 'SQUADRON',
+    },
+    create: {
+      name: '752 OSS',
+      orgTier: 'SQUADRON',
     },
   });
   //#endregion
@@ -35,14 +74,56 @@ async function main() {
     }
   }
 
+  try {
+    await prisma.refreshToken.deleteMany({
+      where: { user: { email: CCEmail } },
+    });
+    await prisma.user.delete({ where: { email: CCEmail } });
+  } catch (e) {
+    //delete can fail if no entities are found. Ignore that
+    if (!(e instanceof PrismaClientKnownRequestError)) {
+      throw e;
+    }
+  }
+
+  try {
+    await prisma.refreshToken.deleteMany({
+      where: { user: { email: 'michael.henry.2@us.af.mil' } },
+    });
+    await prisma.user.delete({ where: { email: 'michael.henry.2@us.af.mil' } });
+  } catch (e) {
+    //delete can fail if no entities are found. Ignore that
+    if (!(e instanceof PrismaClientKnownRequestError)) {
+      throw e;
+    }
+  }
+
+  try {
+    await prisma.refreshToken.deleteMany({
+      where: { user: { email: 'henry.henderson.99@us.af.mil' } },
+    });
+    await prisma.user.delete({
+      where: { email: 'henry.henderson.99@us.af.mil' },
+    });
+  } catch (e) {
+    //delete can fail if no entities are found. Ignore that
+    if (!(e instanceof PrismaClientKnownRequestError)) {
+      throw e;
+    }
+  }
+
   if (
     process.env.NODE_ENV !== 'PRODUCTION' &&
     process.env.NX_DEV_ACCOUNT_PASSWORD
   ) {
     const pw = await hash(process.env.NX_DEV_ACCOUNT_PASSWORD, 10);
 
-    const user = await prisma.user.create({
-      data: {
+    await prisma.user.upsert({
+      where: {
+        email: 'admin@admin.com',
+      },
+      update: {},
+      create: {
         email: 'admin@admin.com',
         password: pw,
         orgs: { connect: { name: 'Scorpion Developers' } },
@@ -52,9 +133,87 @@ async function main() {
         rank: 'ADMIN',
       },
     });
-    console.log({ user });
+
+    await prisma.user.upsert({
+      where: {
+        email: 'CCEmail',
+      },
+      update: {},
+      create: {
+        email: CCEmail,
+        password: pw,
+        orgs: { connect: { name: orgName } },
+        role: 'CC',
+        firstName: 'John',
+        lastName: 'Doe',
+        rank: 'Lt. Col.',
+      },
+    });
+
+    await prisma.user.upsert({
+      where: {
+        email: 'michael.henry.2@us.af.mil',
+      },
+      update: {},
+      create: {
+        email: 'michael.henry.2@us.af.mil',
+        password: pw,
+        orgs: { connect: { name: '552 MXS' } },
+        role: 'CC',
+        firstName: 'Michael',
+        lastName: 'Henry',
+        rank: 'Lt. Col.',
+      },
+    });
+
+    await prisma.user.upsert({
+      where: {
+        email: 'henry.henderson.99@us.af.mil',
+      },
+      update: {},
+      create: {
+        email: 'henry.henderson.99@us.af.mil',
+        password: pw,
+        orgs: { connect: { name: '752 OSS' } },
+        role: 'CC',
+        firstName: 'Henry',
+        lastName: 'Henderson',
+        rank: 'Lt. Col.',
+      },
+    });
   }
   //#endregion user admin
+
+  //#region survey
+  //delete existing test surveys
+  const orgNames = [
+    {
+      name: '552 ACNS',
+    },
+    {
+      name: '552 MXS',
+    },
+    {
+      name: '752 OSS',
+    },
+  ];
+
+  try {
+    await prisma.survey.deleteMany({
+      where: {
+        orgs: {
+          some: {
+            OR: orgNames,
+          },
+        },
+      },
+    });
+  } catch (e) {
+    //delete can fail if no entities are found. Ignore that
+    if (!(e instanceof PrismaClientKnownRequestError)) {
+      throw e;
+    }
+  }
 }
 
 main()
