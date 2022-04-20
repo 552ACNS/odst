1e351ee70d021fc09d3ccb914a7fe3eb707fe873
eric.cater <eric.cater@us.af.mil>
2022-04-20 13:48:02 -0500

    schema

--- apps/ods-api/src/user/user.resolver.ts
+++ apps/ods-api/src/user/user.resolver.ts
@@ -1,8 +1,6 @@
-import { Resolver, Parent, ResolveField, Args, Query } from '@nestjs/graphql';
+import { Resolver, Parent, ResolveField, Query } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { OrgGQL, UserGQL } from '@odst/types/ods';
-//import { AccessTokenAuthGuard } from '../auth/guards/accessToken.authGuard';
-// import { UseGuards } from '@nestjs/common';
 
 // fix this when we have a better solution
 // eslint-disable-next-line no-restricted-imports
@@ -12,20 +10,17 @@ import { Role } from '.prisma/ods/client';
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
-  // Refactor this to include Cater's Where command when we implement it
-  // @Query(() => [UserGQL], { name: 'findManyUsers' })
-  // @UseGuards(AccessTokenAuthGuard)
-  // async findManyInOrg() {
-  //   return this.userService.findMany({});
-  // }
-
-  @Query(() => [UserGQL], { name: 'findUsersWithRole' })
-  async findUsersWithRole(@Args('role') role: Role): Promise<UserGQL[]> {
-    return this.userService.findUsersWithRole(role);
+  @Query(() => [UserGQL])
+  async getCommanders(): Promise<UserGQL[]> {
+    return this.userService.findMany({
+      where: {
+        role: Role.CC,
+      },
+    });
   }
 
   @ResolveField(() => [OrgGQL])
-  async orgs(@Parent() user: UserGQL) {
+  async orgs(@Parent() user: UserGQL): Promise<OrgGQL[]> {
     return this.userService.orgs({ id: user.id });
   }
 }
--- apps/ods/src/app/login/login.service.ts
+++ apps/ods/src/app/login/login.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Apollo } from 'apollo-angular';
-import {
-  LoginDocument,
-  LoginMutation,
-  LoginMutationVariables,
-} from './login.generated';
 
 @Injectable({
   providedIn: 'root',
