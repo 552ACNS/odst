ffacd808a268c55d5e7a6a4b7e075d4b9344aa79
eric.cater <eric.cater@us.af.mil>
2022-04-21 11:09:31 -0500

    g

--- apps/ods/src/app/login/login.service.ts
+++ apps/ods/src/app/login/login.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 import {
   LoginDocument,
   LoginMutation,
   LoginMutationVariables,
-} from '../../graphql-generated';
+} from './login.generated';
 
 @Injectable({
   providedIn: 'root',
