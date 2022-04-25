bd4e8b5e5f2a4fcc6c21c410bb795fdede9da549
eric.cater <eric.cater@us.af.mil>
2022-04-25 14:44:04 -0500

    only return surveyResponses in current user's org

--- apps/ods-api/src/surveyResponse/surveyResponse.resolver.ts
+++ apps/ods-api/src/surveyResponse/surveyResponse.resolver.ts
@@ -16,8 +16,10 @@ import {
   SurveyGQL,
   ResponseCount,
   SurveyResponseWhereInput,
+  UserGQL,
 } from '@odst/types/ods';
 import { Public } from '@odst/auth';
+import { GetCurrentUser } from '@odst/shared/nest';
 
 @Resolver(() => SurveyResponseGQL)
 export class SurveyResponseResolver {
@@ -30,6 +32,7 @@ export class SurveyResponseResolver {
     return this.surveyResponseService.findMany({ where });
   }
 
+  //TODO findUnqiue is called from frontend, not sure how to prevent commanders from looking at other orgs' responses
   @Query(() => SurveyResponseGQL, { name: 'findUniqueSurveyResponse' })
   async findUnique(
     @Args('surveyResponseWhereUniqueInput')
@@ -71,28 +74,41 @@ export class SurveyResponseResolver {
   }
 
   @Query(() => ResponseCount, { name: 'ResponseCount' })
-  async ResponseCount(): Promise<ResponseCount> {
-    return this.surveyResponseService.countResponses();
+  async ResponseCount(@GetCurrentUser() user: UserGQL): Promise<ResponseCount> {
+    return this.surveyResponseService
+      .countResponses({
+        //survey where org contains user's id
+        survey: { orgs: { some: { users: { some: { id: user.id } } } } },
+      })
+      .then((counts) => ({
+        unresolved: counts[0],
+        overdue: counts[1],
+        resolved: counts[2],
+      }));
   }
 
   @Query(() => [String], { name: 'getIssuesByStatus' })
-
-  // TODO This line gets the current user ID but it requires a login system to exist first.
-  // async getUnresolvedIssues(@GetCurrentUserId() userId: string): Promise<string[]> {
   async getIssuesByStatus(
-    @Args('resolved') resolved: boolean
+    @Args('resolved') resolved: boolean,
+    @GetCurrentUser() user: UserGQL
   ): Promise<string[]> {
-    // return this.surveyResponseService.getUnresolvedIssues(userId);
-    return this.surveyResponseService.getIssuesByStatus(resolved);
-  }
-
-  @Query(() => SurveyResponseGQL, { name: 'getSurveyResponseData' })
-  async getSurveyResponseData(
-    @Args('surveyResponseWhereUniqueInput')
-    surveyResponseWhereUniqueInput: SurveyResponseWhereUniqueInput
-  ) {
-    return this.surveyResponseService.getSurveyResponseData(
-      surveyResponseWhereUniqueInput
+    return (
+      this.surveyResponseService
+        .findMany({
+          where: {
+            resolution: resolved ? { not: null } : null,
+            survey: { orgs: { some: { users: { some: { id: user.id } } } } },
+          },
+          // select: {
+          //   id: true,
+          // },
+          orderBy: {
+            openedDate: 'asc',
+          },
+        })
+        //Not ideal. frontend is doing some wonky delayed loading. Should just return all issues, or paginate them.
+        //Using the findMany above so as to not repeat stuff in the servicer
+        .then((responses) => responses.map((response) => response.id))
     );
   }
 
