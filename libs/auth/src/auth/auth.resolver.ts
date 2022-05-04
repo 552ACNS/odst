1899277df41b6e1ed47ba3042776441e128b69dd
eric.cater <eric.cater@us.af.mil>
2022-05-04 10:23:54 -0500

    push to login if tokens are bad

--- libs/auth/src/auth/auth.resolver.ts
+++ libs/auth/src/auth/auth.resolver.ts
@@ -2,10 +2,8 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { Tokens } from './dtos/tokens.entity';
 import { LoginUserInput, RefreshLoginInput } from './dtos/login.input';
-import { UseGuards } from '@nestjs/common';
+import { Logger, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local.guard';
-import { GetCurrentUser } from '@odst/shared/nest';
-import { User } from './interfaces/user-service.interface';
 import { Public } from './decorator/public.decorator';
 
 @Resolver()
@@ -21,11 +19,12 @@ export class AuthResolver {
     return this.authService.login(loginUserInput);
   }
 
+  @Public()
   @Mutation(() => Tokens)
   async refreshTokens(
-    @Args('refreshLoginInput') refreshLoginInput: RefreshLoginInput,
-    @GetCurrentUser() user: User
+    @Args('refreshLoginInput') refreshLoginInput: RefreshLoginInput
   ): Promise<Tokens> {
-    return this.authService.refreshTokens(refreshLoginInput, user);
+    Logger.log('refreshToken', 'auth resolver');
+    return this.authService.refreshTokens(refreshLoginInput);
   }
 }
