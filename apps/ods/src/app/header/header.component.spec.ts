b1768660481fe74555b74f2a8dbbe69dc631051b
eric.cater <eric.cater@us.af.mil>
2022-05-04 08:55:55 -0500

    fixing tests

--- apps/ods/src/app/header/header.component.spec.ts
+++ apps/ods/src/app/header/header.component.spec.ts
@@ -1,5 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -9,6 +15,16 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
+      imports: [
+        CommonModule,
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatMenuModule,
+        RouterTestingModule,
+      ],
     }).compileComponents();
   });
 
