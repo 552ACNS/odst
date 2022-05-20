1f79fcb7f6034479fdfd7bc22fec72b305956d26
Yuri Sim <yuriosim@gmail.com>
2022-05-20 12:13:09 -0500

    ignored old entity fields

--- libs/types/ods/src/input/comment.unique.input.ts
+++
@@ -1,7 +0,0 @@
-import { InputType } from '@nestjs/graphql';
-import { Prisma } from '.prisma/ods/client';
-
-@InputType()
-export class CommentWhereUniqueInput implements Prisma.CommentWhereUniqueInput {
-  id?: string;
-}
--- libs/types/ods/src/input/comment.update.input.ts
+++
@@ -1,8 +0,0 @@
-import { InputType, PartialType } from '@nestjs/graphql';
-import { Prisma } from '.prisma/ods/client';
-import { CommentCreateInput } from './comment.create.input';
-
-@InputType()
-export class CommentUpdateInput
-  extends PartialType(CommentCreateInput)
-  implements Prisma.CommentUpdateInput {}
