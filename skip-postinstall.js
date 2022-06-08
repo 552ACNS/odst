e4d43e002682a719d817fd3a51a5249329814c1f
eric.cater <eric.cater@us.af.mil>
2022-06-08 11:04:02 -0500

    h

--- skip-postinstall.js
+++ skip-postinstall.js
@@ -2,4 +2,8 @@
 
 console.log(process.env.SKIP_POSTINSTALL);
 
-if (!process.env.SKIP_POSTINSTALL) process.exit(1);
+if (!process.env.SKIP_POSTINSTALL)
+  console.log("if you can see this, why isn't it failing?");
+
+if (!process.env.SKIP_POSTINSTALL || process.env.SKIP_POSTINSTALL === 1)
+  process.exit(1);
