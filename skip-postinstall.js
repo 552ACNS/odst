1d2fee3d49ca4be0314be5cbf4a977497d8e720b
eric.cater <eric.cater@us.af.mil>
2022-06-08 11:21:08 -0500

    --ignore-scripts

--- skip-postinstall.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-console.log(process.env.SKIP_POSTINSTALL);
-
-if (!process.env.SKIP_POSTINSTALL)
-  console.log("if you can see this, why isn't it failing?");
-
-if (!process.env.SKIP_POSTINSTALL || process.env.SKIP_POSTINSTALL === 1)
-  process.exit(1);
