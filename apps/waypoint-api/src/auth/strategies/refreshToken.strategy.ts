d648023ea697cb429e404f96c718ee224ffea046
eric.cater <eric.cater@us.af.mil>
2022-06-10 13:20:08 -0500

    fixed more

--- apps/waypoint-api/src/auth/strategies/refreshToken.strategy.ts
+++ apps/waypoint-api/src/auth/strategies/refreshToken.strategy.ts
@@ -30,8 +30,6 @@ export class RefreshTokenStrategy extends PassportStrategy(
   //callback after validation is complete
   async validate(req: Request, payload: JwtPayload) {
     const refreshToken = req.headers['authorization']?.replace('Bearer ', '');
-    //this probably is pointless, since we're in the callback
-    if (!refreshToken) throw new BadRequestException();
 
     //TODO should this be done/happening here?
     const user = await this.userService.findUnique({
