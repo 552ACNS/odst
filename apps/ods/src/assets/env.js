271c1df920fc257a0286d7cc5d911606a194b2b5
eric.cater <eric.cater@us.af.mil>
2022-06-16 11:32:00 -0500

    print env

--- apps/ods/src/assets/env.js
+++ apps/ods/src/assets/env.js
@@ -1,6 +1,6 @@
 (function (window) {
   window['env'] = window['env'] || {};
-
+  console.log(window['env']);
   // Environment variables
   window['env']['NX_GQL_ENDPOINT'] = 'http://localhost:3343/graphql';
   window['env']['production'] = true;
