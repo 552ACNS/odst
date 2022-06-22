03f8280e405257a28f2443e1e3d517bccaffe189
BrendenMcGrew <brendenmcgrew658@gmail.com>
2022-06-22 09:49:48 -0500

    Made a working test

--- apps/ods/src/app/responses/select-tags/select-tags.component.spec.ts
+++ apps/ods/src/app/responses/select-tags/select-tags.component.spec.ts
@@ -1,22 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResponsesModule } from '../responses.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SelectTagsComponent } from './select-tags.component';
+import { MatChipsModule } from '@angular/material/chips';
+import { FormControl } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('SelectTagsComponent', () => {
   let component: SelectTagsComponent;
   let fixture: ComponentFixture<SelectTagsComponent>;
   let compiled;
+  let tagCtrl: FormControl;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ResponsesModule],
+      imports: [ResponsesModule, BrowserAnimationsModule, MatChipsModule],
       declarations: [SelectTagsComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
+    tagCtrl = new FormControl();
     fixture = TestBed.createComponent(SelectTagsComponent);
     component = fixture.componentInstance;
+    component.selectedTags = ['Tag 1', 'Tag 2', 'Tag 3'];
+    component.tagCtrl = tagCtrl;
     compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
@@ -26,9 +34,14 @@ describe('SelectTagsComponent', () => {
   });
 
   it('should populate with selected tags', () => {
-    component.selectedTags = ['Tag 1', 'Tag 2', 'Tag 3'];
-    const displayedTags = compiled.querySelectorAll('mat-chip');
+    const displayedTags = fixture.debugElement
+      .queryAll(By.css('.mat-chip'))
+      .map((Tag) =>
+        Tag.nativeElement.textContent.replace(' cancel', '').trim()
+      );
+    // const test = displayedTags[0].nativeElement.textContent;
+    // expect(displayedTags.length()).toEqual(3);
+    expect(displayedTags.length).toEqual(component.selectedTags?.length);
     expect(displayedTags).toEqual(component.selectedTags);
-    expect(displayedTags.length()).toEqual(3);
   });
 });
