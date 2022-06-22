bbbb3e3c74f3e8d437b5420597d217c8501fab32
Yuri Sim <yuriosim@gmail.com>
2022-06-22 09:37:16 -0500

    reworded questions

--- apps/ods/src/app/feedback-questions/feedback-questions.component.ts
+++ apps/ods/src/app/feedback-questions/feedback-questions.component.ts
@@ -10,10 +10,10 @@ import { FeedbackQuestionsService } from './feedback-questions.service';
 })
 export class FeedbackQuestionsComponent implements OnInit, OnDestroy {
   questions = [
-    'What squadron did the event occur in?',
-    'Please describe the event of a microaggression or discrimination that took place in your squadron. Please refrain from using names or identifying information.',
+    'What organization did the event occur in?',
+    'Please describe the event of a microaggression or discrimination that took place in your organization. Please refrain from using names or identifying information.',
     'Was the person performing the microaggression or discrimination active duty, civilian, guard/reserve or a contractor?',
-    'Who is your SQ/CC?',
+    'Who is your Commander?',
     'Are you active duty, a civilian, guard/reserve or a contractor?',
     'What impacts did this event have on you or your work environment?',
   ];
