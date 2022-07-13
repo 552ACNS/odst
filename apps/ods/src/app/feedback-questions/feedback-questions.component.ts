8c3c8ceb7e3ab4335787c50d19e7366a2d6166ad
BrendenMcGrew <brendenmcgrew658@gmail.com>
2022-07-13 09:09:18 -0500

    No longer hardcoded attached org

--- apps/ods/src/app/feedback-questions/feedback-questions.component.ts
+++ apps/ods/src/app/feedback-questions/feedback-questions.component.ts
@@ -90,7 +90,7 @@ export class FeedbackQuestionsComponent implements OnInit, OnDestroy {
     // TODO creating the survey should be done via prisma seed or via user input
     this.feedbackService
       .submitWithQuestions(this.questions, {
-        name: '552 ACW',
+        name: this.form.get(['eventOrg'])?.value,
       })
       .subscribe(({ data }) => {
         this.feedbackID = data?.createFeedbackWithQuestions;
