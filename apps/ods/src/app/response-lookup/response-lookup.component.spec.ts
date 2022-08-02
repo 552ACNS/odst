0e7a36b277eb5cedc5d9a08e6bf6a693c2bbaba2
BrendenMcGrew <brendenmcgrew658@gmail.com>
2022-08-02 14:55:14 -0500

    Added unit tests for lookupResponse

--- apps/ods/src/app/response-lookup/response-lookup.component.spec.ts
+++ apps/ods/src/app/response-lookup/response-lookup.component.spec.ts
@@ -1,4 +1,9 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  TestBed,
+  ComponentFixture,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
@@ -7,33 +12,100 @@ import {
 } from 'apollo-angular/testing';
 
 import { ResponseLookupComponent } from './response-lookup.component';
+import { ResponseLookupService } from './response-lookup.service';
 import { ResponseLookupModule } from './response-lookup.module';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { of } from 'rxjs';
 
 describe('ResponsesComponent', () => {
-  // let fixture: ComponentFixture<ResponsesComponent>;
-  let controller: ApolloTestingController;
+  let component: ResponseLookupComponent;
+  let fixture: ComponentFixture<ResponseLookupComponent>;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [ResponseLookupComponent],
-      providers: [ResponseLookupModule],
+      providers: [
+        { provide: ResponseLookupService, useValue: mockResponseLookupService },
+      ],
       imports: [
         ApolloTestingModule,
+        RouterTestingModule,
         BrowserAnimationsModule,
         ReactiveFormsModule,
+        ResponseLookupModule,
+        MatCardModule,
+        MatInputModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatListModule,
       ],
     }).compileComponents();
-
-    controller = TestBed.inject(ApolloTestingController);
-    // fixture.detectChanges();
   });
+
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(ResponseLookupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  const response = {
+    data: {
+      feedbackResponseByID: {
+        openedDate: '2022-07-28T18:51:06.321Z',
+        closedDate: '2022-07-28T18:51:18.893Z',
+        resolved: true,
+        tags: ['Brought in external agency to educate'],
+        firstName: 'Admin',
+        lastName: 'Admin',
+        grade: 'E-∞',
+        __typename: 'TrackedFeedback',
+      },
+    },
+  };
+
+  const mockResponseLookupService = {
+    getFeedbackReponseById: jest.fn(),
+  };
 
   it('should create', () => {
-    expect(true).toBeTruthy();
-    // expect(component).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
+  it('should submit a response ID to lookup', fakeAsync(() => {
+    mockResponseLookupService.getFeedbackReponseById = jest
+      .fn()
+      .mockReturnValue(of(response));
+    component.form.setValue({ reportID: 'Mock ID' });
+    component.submit();
+    fixture.detectChanges();
+    tick();
+
+    const expected = response.data.feedbackResponseByID;
+    const actual = {
+      openedDate: component.openedDate,
+      closedDate: component.closedDate,
+      resolved: component.status,
+      tags: component.tags,
+      firstName: component.firstName,
+      lastName: component.lastName,
+      grade: component.grade,
+      __typename: 'TrackedFeedback',
+    };
+
+    expect(actual).toEqual(expected);
+  }));
+
   afterEach(() => {
-    controller.verify();
+    if (fixture) {
+      fixture.destroy();
+    }
   });
 });
