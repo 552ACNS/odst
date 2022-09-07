27da472273daad268b4f2a75e8dabde70a7708bf
BrendenMcGrew <brendenmcgrew658@gmail.com>
2022-09-07 11:09:20 -0500

    Added jest and cypress tests

--- apps/ods/src/app/password-recovery/password-recovery.component.spec.ts
+++ apps/ods/src/app/password-recovery/password-recovery.component.spec.ts
@@ -1,18 +1,61 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { PasswordRecoveryComponent } from './password-recovery.component';
+import { PasswordRecoveryService } from './password-recovery.service';
+import { PasswordRecoveryModule } from './password-recovery.module';
+import { of } from 'rxjs';
 
 describe('PasswordRecoveryComponent', () => {
   let component: PasswordRecoveryComponent;
   let fixture: ComponentFixture<PasswordRecoveryComponent>;
 
+  const mockPasswordRecoveryService = {
+    passwordRecovery: jest.fn(),
+  };
+
+  const response = {
+    data: {
+      email: 'email@a.com',
+    },
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PasswordRecoveryComponent],
+      providers: [
+        {
+          provide: PasswordRecoveryService,
+          useValue: mockPasswordRecoveryService,
+        },
+      ],
+      imports: [
+        ApolloTestingModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        PasswordRecoveryModule,
+      ],
     }).compileComponents();
+  });
 
+  beforeEach(async () => {
     fixture = TestBed.createComponent(PasswordRecoveryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should make changeSuccess true because a password reset request was successful', async () => {
+    mockPasswordRecoveryService.passwordRecovery = jest
+      .fn()
+      .mockReturnValue(of(response));
+    component.passwordRecoveryForm.setValue({ email: 'admin@admin.com' });
+    component.recoverPassword();
+
+    expect(component.recoveryRequestSubmitted).toBeTruthy();
   });
 });
--- apps/ods/src/app/password-reset/password-reset.component.spec.ts
+++ apps/ods/src/app/password-reset/password-reset.component.spec.ts
@@ -1,22 +1,60 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { PasswordResetModule } from './password-reset.module';
 import { PasswordResetComponent } from './password-reset.component';
+import { PasswordResetService } from './password-reset.service';
+import { of } from 'rxjs';
 
 describe('PasswordResetComponent', () => {
   let component: PasswordResetComponent;
   let fixture: ComponentFixture<PasswordResetComponent>;
 
+  const mockPasswordResetService = {
+    passwordReset: jest.fn(),
+  };
+
+  const response = {
+    data: {
+      password: 'P@ssw0rd',
+    },
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PasswordResetComponent],
+      providers: [
+        { provide: PasswordResetService, useValue: mockPasswordResetService },
+      ],
+      imports: [
+        ApolloTestingModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        PasswordResetModule,
+      ],
     }).compileComponents();
+  });
 
+  beforeEach(async () => {
     fixture = TestBed.createComponent(PasswordResetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should submit a changed password and receive confirmation it was changed because the input boxes were filled with the same password', async () => {
+    mockPasswordResetService.passwordReset = jest
+      .fn()
+      .mockReturnValue(of(response));
+    component.passwordResetForm.setValue({
+      newPassword: 'p@ssw0rd',
+      confirmPassword: 'p@ssw0rd',
+    });
+    component.resetPassword();
+    expect(component.changeSuccess).toBeTruthy();
   });
 });
