1b40cd37460073d26e42152eea234acbf8b98d78
Andydawg313 <Andydawg313@gmail.com>
2022-11-17 12:54:48 -0600

    resetfield

--- apps/ods/src/app/components/comments/comments.component.ts
+++ apps/ods/src/app/components/comments/comments.component.ts
@@ -1,29 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { GetReportByStatusQuery } from '../../responses/responses.generated';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CustomValidators } from '@odst/shared/angular';
 
 @Component({
   selector: 'odst-comments',
   standalone: true,
   imports: [
     CommonModule,
-    MatButtonModule,
     MatCardModule,
     MatTooltipModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
+    MatButtonModule,
     MatInputModule,
   ],
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent implements OnInit {
-  constructor() {}
+  @Input() comments: GetReportByStatusQuery['getIssuesByStatus'][0]['comments'];
+  @Input() userId: string;
+
+  // addComment = new FormGroup({
+  newComment = new FormControl('', [
+    Validators.required,
+    CustomValidators.noWhitespaceValidator,
+  ]);
+  // });
+
+  constructor() {
+    console.log('CommentsComponent');
+  }
 
   ngOnInit(): void {
     console.log('CommentsComponent');
+  }
+
+  submitComment() {
+    console.log('submitComment');
+  }
+
+  resetField() {
+    // if the field is empty or only contains whitespace, reset it
+    if (this.newComment.value === '' || this.newComment.value === null) {
+      this.newComment.reset();
+    }
   }
 }
