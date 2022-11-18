da0cf1b0be9f33b1cb859427ebbfe40d1eb01ff6
Andydawg313 <Andydawg313@gmail.com>
2022-11-18 12:05:07 -0600

    sherritt huckims comment tests

--- apps/ods/src/app/components/comments/comments.component.spec.ts
+++ apps/ods/src/app/components/comments/comments.component.spec.ts
@@ -1,14 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { Role } from 'apps/ods/src/types.graphql';
 import { GetReportByStatusQuery } from '../../responses/responses.generated';
 
 import { CommentsComponent } from './comments.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+
 describe('CommentsComponent', () => {
   let component: CommentsComponent;
   let fixture: ComponentFixture<CommentsComponent>;
 
-  let comments: GetReportByStatusQuery['getIssuesByStatus'][0]['comments'] = [
+  const comments: GetReportByStatusQuery['getIssuesByStatus'][0]['comments'] = [
     {
       value: 'test comment',
       date: '2021-05-05T00:00:00.000Z',
@@ -19,11 +23,31 @@ describe('CommentsComponent', () => {
         role: Role.Admin,
       },
     },
+    {
+      value: 'test comment',
+      date: '2021-05-05T00:00:00.000Z',
+      author: {
+        id: '2',
+        firstName: 'Second',
+        lastName: 'Last',
+        role: Role.Admin,
+      },
+    },
+    {
+      value: 'test comment 2',
+      date: '2021-05-06T00:00:00.000Z',
+      author: {
+        id: '3',
+        firstName: 'Third',
+        lastName: 'Last',
+        role: Role.Cc,
+      },
+    },
   ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CommentsComponent],
+      imports: [CommentsComponent, BrowserAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CommentsComponent);
@@ -32,8 +56,8 @@ describe('CommentsComponent', () => {
   });
 
   it('should create', () => {
-    // expect(component).toBeTruthy();
-    expect(true).toBeTruthy();
+    expect(component).toBeTruthy();
+    // expect(true).toBeTruthy();
   });
 
   it('should display comments', () => {
@@ -42,12 +66,41 @@ describe('CommentsComponent', () => {
 
     // find the elements that have the class of `text-sm text-left whitespace-pre-line`
     const commentElements = fixture.nativeElement.querySelectorAll(
-      '.text-sm.text-left.whitespace-pre-line'
+      'text-sm text-left whitespace-pre-line'
     );
 
-    expect(true).toBeTruthy();
+    expect(commentElements).toBeTruthy();
 
     // check that the comment is displayed
     // expect(commentElements[0].textContent).toContain(comments[0].value);
+  });
+
+  it('submit a new comment when the button is clicked', () => {
+    // set the value of the comment
+    component.newComment.setValue('test comment');
+    // const textarea = fixture.debugElement.query(By.css('#comment'));
+    // textarea.nativeElement.value = 'test comment';
+    fixture.detectChanges();
+    // find the button element
+    const button = fixture.nativeElement.querySelectorAll(
+      'w-full text-white shadow-md rounded-full'
+    );
+    // click the button
+    expect(button).toBeTruthy();
+    fixture.detectChanges();
+
+    // check that the comment was submitted
+    expect(component.commentToSubmit.emit).toHaveBeenCalledWith('test comment');
+  });
+
+  it('should display the date of the first comment', () => {
+    component.comments = comments;
+    fixture.detectChanges();
+
+    // The date objects have a margin of mx-4
+    const dateElements = fixture.debugElement.queryAll(By.css('.mx-4'));
+
+    // Given that we have a non-zero amount of comments, there should always be a date
+    expect(dateElements[0].nativeElement.textContent).toEqual('May 4, 2021');
   });
 });
